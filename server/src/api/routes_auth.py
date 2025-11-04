d769071aeecb7eef0a44b62aec3fc3a759f288b7
Alemayehu <alemayehudabi606@gmail.com>
2025-11-04 18:33:48 +0300

    wip: login endpoint

--- server/src/api/routes_auth.py
+++ server/src/api/routes_auth.py
@@ -3,7 +3,7 @@ from src.core.auth_schema import Register, Login, UserResponse
 from sqlalchemy.orm import Session
 from src.core.db import get_db
 from src.core.model import User
-from src.utils.hash_verify import hashPassword
+from src.utils.hash_verify import hashPassword, verifyPassword
 
 router  = APIRouter()      
            
@@ -24,10 +24,25 @@ def register(data:Register, db:Session = Depends(get_db)):
    
    return {
       "id": new_user.id,
+      "name": new_user.name,
       "email": new_user.email
    }
                     
             
 @router.post("/login")    
-async def login (data:Login):
-    pass
+async def login (data:Login, db:Session = Depends(get_db)):
+   is_user_exist = db.query(User).filter(User.email == data.email).first()
+   if not is_user_exist:
+       raise HTTPException(400, "Invalid credentials.")
+   
+   password_hashed = verifyPassword(data.password, is_user_exist.password)
+   
+   if not password_hashed:
+       raise HTTPException(400, "Invalid credentials.")
+      
+   return {
+      "id": is_user_exist.id,
+      "name": is_user_exist.name,
+      "email": is_user_exist.email,
+      "role": is_user_exist.role 
+   }
--- server/src/core/db.py
+++ server/src/core/db.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker
 from src.core.config import settings
 from src.core.model import Base 
 
-print("DATABASE_URL", settings.DATABASE_URL)
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
